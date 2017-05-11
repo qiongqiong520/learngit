d933b47273dab47064fb4afd9a03ee2b5a2e16df
qiongqiong520 <1153478519@qq.com>
2017-05-11 23:48:45 +0800

    sherry

---
+++ 20170510/7652整形数据存储空间大小.cc
@@ -0,0 +1,19 @@
+ ///
+ /// @file    7562整形数据类型存储空间大小.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2017-05-10 10:44:37
+ ///
+ 
+#include <iostream>
+#include<cstdio>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int a;
+	short b;
+	printf("%d %d",sizeof(a),sizeof(b));
+	return 0;
+}
---
+++ 20170510/7895字符菱形.cc
@@ -0,0 +1,23 @@
+ ///
+ /// @file    7895字符菱形.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2017-05-10 10:43:36
+ ///
+ 
+#include <iostream>
+#include<cstdio>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	char c;
+	cin>>c;
+	cout<<" "<<c<<endl;
+	cout<<" "<<c<<c<<c<<endl;
+	cout<<c<<c<<c<<c<<c<<endl;
+	cout<<" "<<c<<c<<c<<endl;
+	cout<<" "<<c<<endl;
+	return 0;
+}
